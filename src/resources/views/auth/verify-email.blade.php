1492a185d3fa9f88e618225cedbe8e8828932166
moriyama masahiro <masa19930317@gmail.com>
2025-07-18 00:04:34 +0900

    20250717_森山_メール認証画面css

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -7,10 +7,8 @@
 @section('content')
   <div class="verify-email__content">
     <div class="verify-email__item">
-      <p class="verify-email__message">
-        登録していただいたメールアドレスに認証メールを送信しました。<br>
-        メール認証を完了してください。
-      </p>
+      <p class="verify-email__message">登録していただいたメールアドレスに認証メールを送信しました。</p>
+      <p class="verify-email__message">メール認証を完了してください。</p>
 
       @if (session('status') == 'verification-link-sent')
         <div class="verify-email__status">
@@ -24,7 +22,7 @@
 
       <form action="{{ route('logout') }}" method="post">
         @csrf
-        <div class="verify-email__actions">
+        <div class="verify-email__retry">
           <button type="submit" class="verify-email__button">
             認証メールを再送する
           </button>
